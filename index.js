cd6479b1aa68f7b5209369819febc2d0aa7ccb63
Hirshey <noah@thehirschfields.com>
2022-10-16 17:52:38 -0400

    fix await/promise

--- index.js
+++ index.js
@@ -11,29 +11,32 @@ exports.handler = async (event) => {
             'Content-Type': 'application/json'
         }
       };
-      
-      var req = https.request(options, function (res) {
-        var chunks = [];
-      
-        res.on("data", function (chunk) {
-          chunks.push(chunk);
+      let promise = new Promise((resolve, reject) => {
+        var req = https.request(options, function (res) {
+            var chunks = [];
+        
+            res.on("data", function (chunk) {
+            chunks.push(chunk);
+            });
+        
+            res.on("end", function (chunk) {
+                var body = Buffer.concat(chunks);
+                resolve(body.toString());
+            });
+        
+            res.on("error", function (error) {
+            reject(error);
+            });
         });
-      
-        res.on("end", function (chunk) {
-          var body = Buffer.concat(chunks);
-          console.log(body.toString());
-        });
-      
-        res.on("error", function (error) {
-          console.error(error);
-        });
-      });
-      
-      req.write(JSON.stringify({
-        text: `Issue Created: ${event.issue.html_url}`
-      }));
+        
+        req.write(JSON.stringify({
+            text: `Issue Created: ${event.issue.html_url}`
+        }));
 
-      req.end();
+        req.end();
+    });
+    
+    await promise;
 
     const response = {
         statusCode: 200
